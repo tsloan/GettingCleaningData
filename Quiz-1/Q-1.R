04b82c53fafc64d140fd7e251f5136ccf86da0cf
tsloan <terry@sloandrums.com>
2014-06-06 17:25:41 +0100

    Working version

--- Quiz-1/Q-1.R
+++ Quiz-1/Q-1.R
@@ -14,16 +14,48 @@
 ##
 ###############################################################################
 
+###############################################################################
+## Set path on my laptop
+###############################################################################
 
+setwd("C:\\Terry-Programming\\Coursera\\GettingCleaningData\\Quiz-1")
 
-getdata.data.ss06hid <-
-read.csv("C:/Terry-R-stuff/Coursera-R/GettingAndCleaningData/Quiz-1/getdata-data-ss06hid.csv")
-download.file(
-    "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv",
-    destfile)
-#data<-getdata.data.ss06hid
-#data<-read.csv(getdata.data.ss06hid.csv)
-bad<-is.na(data$VAL)
-gooddata <- data[!bad,]
-nrow(gooddata)
-nrow(gooddata[gooddata$VAL==24,])
+###############################################################################
+## Create directory to hold the data
+###############################################################################
+
+if (!file.exists("data-Q1")){
+    
+    dir.create("data-Q1")
+    
+    ###########################################################################
+    ## Download the file into the Create directory 
+    ###########################################################################
+    
+    fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+    
+    download.file(fileUrl, destfile = "./data-Q1/USCommunitySurvey.csv")
+    
+    list.files("./data-Q1")
+    dateDownloaded <- date()
+    dateDownloaded
+}
+
+###############################################################################
+## Load the data into a data frame
+###############################################################################
+
+SurveyData <- read.csv("./data-Q1/USCommunitySurvey.csv", header=TRUE,)
+
+###############################################################################
+# Property Value is in the VAL field
+# houses of $1M or over have the field value 24
+###############################################################################
+
+bad<-is.na(SurveyData$VAL)
+SurveyData <- SurveyData[!bad,]
+
+houses<-nrow(SurveyData[SurveyData$VAL == 24,])
+
+print(houses)
+
